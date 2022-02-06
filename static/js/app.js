7ef383dd410a4c4ea025da42a891e41c5bbb0086
Fidel Tercero <hueso@Fidels-MBP.attlocal.net>
2022-02-05 23:08:36 -0800

    Set the webpage background to black and text to white and add image using javascript and css

--- static/js/app.js
+++ static/js/app.js
@@ -18,14 +18,15 @@ function buildTable(data) {
         Object.values(dataRow).forEach((val) => {
             let cell = row.append("td");
             cell.text(val);
-        });
+            }
+        );
     });
 }
 
 function handleClick() {
-    // Grab the datetime value from the filter
+    // Grab the datetime value from the filter 11.5.3
     let date = d3.select("#datetime").property("value");
-    let filterData = tableData;
+    let filteredData = tableData;
 
     // Check to see if a date was entered and filter the
     // data using that date.
